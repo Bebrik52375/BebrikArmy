d599dde653384206598fd8bd7a6ee49378f45eb9
Tyuiu.SbrodovSV@outlook.com <Tyuiu.SbrodovSV@outlook.com>
2023-09-21 21:42:39 +0500

    Sprint1.Task2.V1

---
+++ Tyuiu. ___YOURNAME___ .Sprint1.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.___YOURNAME___.Sprint1.Task2.V1.Lib;
+
+namespace Tyuiu.___YOURNAME___.Sprint1.Task2.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int value = 1;
+            var res = ds.ConvertKmToM(value);
+            Assert.AreEqual(1.609, res);
+        }
+    }
+}
