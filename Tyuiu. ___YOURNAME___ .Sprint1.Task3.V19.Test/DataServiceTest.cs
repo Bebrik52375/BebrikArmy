faaebfbfd5dac1ad78e9064acde79e5e83e8df38
Tyuiu.SbrodovSV@outlook.com <Tyuiu.SbrodovSV@outlook.com>
2023-09-22 22:49:09 +0500

    Sprint1.Task3.V19

---
+++ Tyuiu. ___YOURNAME___ .Sprint1.Task3.V19.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.___YOURNAME___.Sprint1.Task3.V19.Lib;
+
+namespace Tyuiu.___YOURNAME___.Sprint1.Task3.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService dataService = new DataService();
+            Assert.AreEqual(true, dataService.ElephCanMove(4, 7, 5, 8));
+        }
+    }
+}
